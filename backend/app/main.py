e5bff86657e5cd9fb439c14e7f1b0da85501c3fd
shikhar0718 <shikhar0718@gmail.com>
2026-01-10 22:54:17 +0530

    accepting every api request from any origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,7 +36,7 @@ app=FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://plantdiseaseprediction.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
